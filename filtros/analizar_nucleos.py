1828fca3ac68be672507a5bfbdb07db3be5911f0
AracelyZubieta <aracely.zubieta@ucb.edu.bo>
2025-12-06 14:18:03 -0400

    Create analizar_nucleos.py
    analizar nucleos

---
+++ filtros/analizar_nucleos.py
@@ -0,0 +1,19 @@
+def analizar_nucleos(labels_ws, sharpened_im):
+    props = regionprops(labels_ws, intensity_image=sharpened_im)
+
+    areas = []
+    intensidades = []
+
+    for r in props:
+        areas.append(r.area)
+        intensidades.append(r.mean_intensity)
+
+    areas = np.array(areas)
+    intensidades = np.array(intensidades)
+
+    print("Número de núcleos detectados:", len(props))
+    print("Área mínima:", np.min(areas))
+    print("Área máxima:", np.max(areas))
+    print("Área promedio:", np.mean(areas))
+
+    return props, areas, intensidades
